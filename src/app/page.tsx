598a04f9ff88a57a6a08fa9a2e976f2b902ff0c9
Tony <tonywu3758@gmail.com>
2024-03-09 13:43:58 -0800

    add dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,29 @@ import Image from "next/image";
 import eee from "./page.module.css";
 import AbcTest from "./abc";
 import DropDownMenu from "./components/DropDownMenu";
-
+import page_css from "./page.module.css";
 export default function Home() {
   const handleOptionClick = (option: string) => {
     console.log(option);
   };
 
-  const options = [
+  const class_options = [
     {
-      text: "Option 1",
+      text: "Math",
       onClick: handleOptionClick,
     },
-    { text: "Option 2", 
-      onClick: handleOptionClick },
+    { 
+      text: "Reading", 
+      onClick: handleOptionClick 
+    },
+    {
+      text: "Writing",
+      onClick: handleOptionClick
+    }
   ];
 
   return (<main>
-      <DropDownMenu options={options} defaultText={"Classes"}/>
-      <AbcTest/>
+    <p className={page_css.title}> Academic Ally</p>
+    <DropDownMenu options={class_options} defaultText={"Subjects"}/>
   </main>);
 }
