cbdbb8bb14d01544fcf3d9a51bbf6502a81f3999
Tony <tonywu3758@gmail.com>
2024-03-09 18:33:29 -0800

    other stufff

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,9 +1,22 @@
 "use client";
-import css from "./page.module.css";
+import page_css from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <p className={css.title}>Algebra 2, Lesson 1</p>
+      <div className={page_css.div}>
+        <p className={page_css.title}>Explanation:</p>
+        <img className={page_css.load} src="/loadinggif.gif"></img>
+      </div>
+      <p className={page_css.text}>text</p>
+      <div className={page_css.div2}>
+        <input
+          className={page_css.input}
+          placeholder="add your response here: "
+        ></input>
+        <br></br>
+        <br />
+        <button className={page_css.submit}>Submit</button>
+      </div>
     </main>
   );
 }
