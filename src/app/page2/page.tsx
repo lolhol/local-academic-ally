f716d9ccc01ea006b2848949109b3b60ea01ab9c
Tony <tonywu3758@gmail.com>
2024-03-09 18:59:19 -0800

    qqq
    Co-authored-by: godbrigero <lolhol@users.noreply.github.com>

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,12 +1,36 @@
 "use client";
+import { useEffect, useState } from "react";
 import page_css from "./page.module.css";
 export default function Home() {
+  const [isRenderLoad, setRenderLoad] = useState(true);
+
+  const fetchCourses = async () => {
+    try {
+      const response = await fetch("/api/getResponseAI");
+      while (true) {
+        if (response == undefined) {
+          continue;
+        } else {
+          setRenderLoad(false);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
   return (
     <main>
-      <div className={page_css.div}>
-        <p className={page_css.title}>Explanation:</p>
-        <img className={page_css.load} src="/loadinggif.gif"></img>
-      </div>
+      {isRenderLoad && (
+        <div className={page_css.div}>
+          <p className={page_css.title}>Explanation:</p>
+          <img className={page_css.load} src="/loadinggif.gif"></img>
+        </div>
+      )}
       <p className={page_css.text}>text</p>
       <div className={page_css.div2}>
         <input
